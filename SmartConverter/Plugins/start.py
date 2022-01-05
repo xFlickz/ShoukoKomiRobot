a2feb578f9dfcc4e0ed3d38d93553168487fdef3
Parth-Senpai <weebdark823@gmail.com>
2022-01-05 19:07:50 +0530

    hmm yeah

--- SmartConverter/Plugins/start.py
+++ SmartConverter/Plugins/start.py
@@ -23,11 +23,11 @@ async def start_cmd_handler(bot, message):
   )
   
 @TGBot.on_callback_query()
-async def helpo(bot, m):
-  if m.data == "help":
-    await m.message.delete()
+async def helpo(bot, message):
+  if message.data == "help":
+    await message.message.delete()
     await bot.send_video(
-      chat_id=m.message.chat.id,
+      chat_id=message.message.chat.id,
       video="https://telegra.ph/file/33186a24917037de0d97a.mp4",
       text=Translation.HELP_TEXT,
       parse_mode="markdown",
@@ -37,10 +37,10 @@ async def helpo(bot, m):
         ],
       )
     )
-  elif m.data == "about":
-    await m.message.delete()
+  elif message.data == "about":
+    await message.message.delete()
     await bot.send_video(
-      chat_id=m.message.chat.id,
+      chat_id=message.message.chat.id,
       video="https://telegra.ph/file/a7f47db6297329258755e.mp4",
       caption="**LANGUAGE** [Python](https://www.python.org)\n**LIBRARY** [Pyrogram](https://www.pyrogram.org)\n**SOURCE CODE** [Click me](https://t.me/Shity_man)\n**DEV** [Dark](https://t.me/Bro_isDarkal)",
       parse_mode="markdown",
@@ -50,10 +50,10 @@ async def helpo(bot, m):
         ],
       )
     )
-  elif m.data == "back":
-    await m.message.delete()
+  elif message.data == "back":
+    await message.message.delete()
     await bot.send_video(
-      chat_id=m.message.chat.id,
+      chat_id=message.message.chat.id,
       video="https://telegra.ph/file/f7006e236e28ba090d407.mp4",
       caption=Translation.START_TEXT,
       reply_markup=InlineKeyboardMarkup(
