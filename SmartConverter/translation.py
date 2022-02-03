19d4ea4c467fc8ec3a5453e18624e071c2904842
GENZxM <97391354+GENZxM@users.noreply.github.com>
2022-02-03 15:04:37 +0800

    Update translation.py

--- SmartConverter/translation.py
+++ SmartConverter/translation.py
@@ -3,6 +3,6 @@ This Bot Was Developed By The Owner Of @XFlick.
 Join his network and support him.
 """
 class Translation:
-  START_TEXT = "✫Hᴇʟʟᴏ, I ᴀᴍ Miku. I ᴄᴀɴ ᴄᴏɴᴠᴇʀᴛ ᴠᴀʀɪᴏᴜs ғɪʟᴇs ɪᴍ ʏᴏᴜʀ ᴅᴇsɪʀᴇᴅ ғᴏʀᴍᴀᴛ.\nKɪɴᴅʟʏ, Jᴏɪɴ ᴍʏ ɴᴇᴛᴡᴏʀᴋ Too✫"
+  START_TEXT = "✫Hᴇʟʟᴏ, I ᴀᴍ Komi. I ᴄᴀɴ ᴄᴏɴᴠᴇʀᴛ ᴠᴀʀɪᴏᴜs ғɪʟᴇs ɪᴍ ʏᴏᴜʀ ᴅᴇsɪʀᴇᴅ ғᴏʀᴍᴀᴛ.\nKɪɴᴅʟʏ, Jᴏɪɴ ᴍʏ ɴᴇᴛᴡᴏʀᴋ Too✫"
   
   HELP_TEXT = "✫Jᴜsᴛ sᴇɴᴅ ᴍᴇ ᴀ ᴠɪᴅᴇᴏ ᴏʀ ғɪʟᴇ ᴛᴏ ɢᴇᴛ sᴛᴀʀᴛᴇᴅ. I ᴄᴀɴ ᴄᴏɴᴠᴇʀᴛ ᴘᴅғ, ᴇᴘᴜʙ, ᴅᴏᴄx ʟɪʟᴇ ᴅᴏᴄᴜᴍᴇɴᴛs ᴀʟsᴏ.\nI ʜᴀᴠᴇ ᴘᴏᴛᴇɴᴛɪᴀʟ ᴛᴏ ᴄᴏɴᴠᴇʀᴛ ᴍᴘ𝟺 ɪɴ ᴍᴋᴠ, sᴛʀᴇᴀᴍ ᴀɴᴅ ᴠɪᴄᴇ-ᴠᴇʀsᴀ✫"
